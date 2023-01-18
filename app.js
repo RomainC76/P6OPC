32b0cebc72fcb2e1cb550b0f6f4b6452769cad22
Romain <chaventre.romain@orange.fr>
2023-01-18 09:56:51 +0100

    add image

--- app.js
+++ app.js
@@ -21,34 +21,30 @@ const userRoutes = require('./routes/user');
 
 //Connection à la base de données
 mongoose.connect('mongodb+srv://serialcoder:aBQjHHvMgRei93y@picante.kobnz2f.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 //Gestion des erreurs CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 //on accède au corps des requêtes
 //on intercepte toutes les requêtes au format JSON
 // et on les met à disposition dans req.body
 app.use(bodyparser.json());
 
-
-
-//définition de la destination des images
-
-
-
-
 app.use('/api/sauces', productRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 
