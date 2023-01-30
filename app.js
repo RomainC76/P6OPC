8ae40e156984229f37be0f270008bd48c08b5537
Romain <chaventre.romain@orange.fr>
2023-01-30 09:52:59 +0100

    Ajout de expressratelimit

--- app.js
+++ app.js
@@ -13,7 +13,19 @@ const mongoose = require("mongoose"); // Utilisation de Mongoose
 const userRoutes = require("./routes/user"); // Chemin user
 const sauceRoutes = require("./routes/sauce"); // Chemin sauce
 
-const app = express(); // Lance Express sur Node.JS
+const app = express(); // Lance Express sur Node.
+
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 //  Configure l'app à la base de données Mongoose
 mongoose.set("strictQuery", true);
